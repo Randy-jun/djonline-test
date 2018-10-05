27b1003ef89178281e62d208f6d05add928c2993
Randy jun <275212517@qq.com>
2018-10-05 12:03:30 +0800

    modal delete

--- my-vue-dome00/src/components/setting/Groups.vue
+++ my-vue-dome00/src/components/setting/Groups.vue
@@ -1,37 +1,67 @@
 <template>
   <div class="col">
-    Groups
+    <small>总共: <span class="font-italic font-weight-bold">{{count_all}}</span> 条记录</small>
     <div class="row">
       <table class="table table-hover table-responsive-lg">
          <thead class="thead-lights">
             <tr>
-               <th>AID</th>
-               <th>标题</th>
-               <th>Details/Edit/Del</th>
+               <!-- <th>
+                 <div class="custom-control custom-checkbox">
+                  <input type="checkbox" class="custom-control-input" v-on:change="choiceall()" id="allCheck">
+                  <label class="custom-control-label" for="allCheck">全选</label>
+                </div>
+               </th> -->
+               <th>序号</th>
+               <th>组团社ID</th>
+               <th>组团社名称</th>
+               <th>备注</th>
+               <th>操作</th>
             </tr>
          </thead>
-         <tbody v-for="(item,key) in data_list" v-bind:key='item.aid'>
+         <tbody>
             <!-- key 要写到后面才可以 -->
-            <tr>
-               <td>{{item.id}}</td>
-               <td>{{item.name}}</td>
+            <tr v-for="(item, index) in data_list">
+              <!-- <td>
+                 <div class="custom-control custom-checkbox">
+                  <input type="checkbox" class="custom-control-input" v-on:change="choiceall()" v-bind:id="item.id">
+                  <label class="custom-control-label" v-bind:for="item.id"></label>
+                </div>
+               </td> -->
+               <td>{{index + 1}}</td>
+               <td v-bind:id="item.id" >{{item.id}}</td>
+               <td v-bind:id="item.id">{{item.name}}</td>
+               <td v-bind:id="item.id">{{item.remark}}</td>
                <td>
                 <div class="btn-group btn-group-sm">
-                  <button type="button" class="btn btn-primary" v-bind:value="'#a'+key">Details</button>
-                  <button type="button" class="btn btn-warnings">Edit</button>
-                  <button type="button" class="btn btn-danger">Del</button>
+                  <button type="button" class="btn btn-second">编辑</button>
+                  <button type="button" class="btn btn-danger" data-toggle="modal" v-bind:data-target='"#deleteConfirm"+item.id'>删除</button>
+                </div>
+                <div class="modal fade" role="dialog" v-bind:id='"deleteConfirm"+item.id'>
+                  <div class="modal-dialog modal-sm">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title">确认删除吗？</h5>
+                        <!-- <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button> -->
+                      </div>
+                      <!-- <div class="modal-body">
+                        <p>Modal body text goes here.</p>
+                      </div> -->
+                      <div class="modal-footer">
+                        <button class="btn btn-danger btn-sm" data-dismiss="modal" v-on:click="delete_group(item.id)">确认</button>
+                        <button class="btn btn-primary btn-sm" data-dismiss="modal">取消</button>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </td>
             </tr>
          </tbody>
       </table>
-      <div class="row">
-        <div class="col-2"><button class="btn btn-default" v-on:click="doAdd($event)">添加</button></div>
-      </div>
     </div>
     <div class="row">
-        <div class="col"><button class="btn btn-success" v-on:click="postTest(true)">POST test</button></div>
-        <div class="col"><button class="btn btn-danger" v-on:click="postTest(false)">POST test</button></div>
+      <div class="col-2"><button class="btn btn-default" v-on:click="doAdd($event)">添加</button></div>
     </div>
   </div>
 </template>
@@ -46,6 +76,7 @@ export default {
   },
   data() {
     return {
+      count_all:0,
       order:{
         dj_name:"百恒国际旅行社",
         zt_name:"光大旅行社",
@@ -60,18 +91,12 @@ export default {
     }
   },
   methods: {
-    doAdd(e){
-      // console.log(e)
-      if (e.type == 'click' || e.keyCode == 13) {
-        this.list.push(
-          {
-            title:this.todo,
-            checked:false,
-          }
-        );
-      this.todo='';
-      }
-      this.saveList();
+    choice(groupid){
+      // console.log(zuid)
+    },
+    delete_group(groupid){
+      
+      console.log(groupid)
     },
     doDel(delKey){
       this.list.splice(delKey,1 );
@@ -107,8 +132,8 @@ export default {
     
     const api='http://127.0.0.1:9090/acct/agencies/';
     Axios.get(api).then((response)=>{
-        console.log(response.data);
         this.data_list=response.data.result;
+        this.count_all=response.data.result.length;
         // console.log(response.data.result);
         // console.log(typeof(response.data.result));
         // this.test_list=response.data.result;
