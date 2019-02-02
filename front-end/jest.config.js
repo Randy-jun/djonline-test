10ce3fe6540fe7aa46b745ce7b1177bbe8d7c3b6
Randy jun <275212517@qq.com>
2019-02-02 15:51:49 +0800

    create new project

--- front-end/jest.config.js
+++ front-end/jest.config.js
@@ -3,14 +3,12 @@ module.exports = {
     'js',
     'jsx',
     'json',
-    'vue',
-    'ts',
-    'tsx'
+    'vue'
   ],
   transform: {
     '^.+\\.vue$': 'vue-jest',
     '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
-    '^.+\\.tsx?$': 'ts-jest'
+    '^.+\\.jsx?$': 'babel-jest'
   },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'
@@ -21,10 +19,5 @@ module.exports = {
   testMatch: [
     '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
   ],
-  testURL: 'http://localhost/',
-  globals: {
-    'ts-jest': {
-      babelConfig: true
-    }
-  }
+  testURL: 'http://localhost/'
 }
