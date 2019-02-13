458c6bb6f321553b72f5d1faf925831c69c86ec0
williamyang900211 <williamyang0211@outlook.com>
2019-02-13 15:27:48 +0800

    add user

--- djonline/djonline/settings.py
+++ djonline/djonline/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'order',
     'rest_framework',
+    'usermanage',
 ]
 
 MIDDLEWARE = [
@@ -48,11 +49,20 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        #'rest_framework.authentication.SessionAuthentication',
+    )
+}
+
+
 
 CORS_ORIGIN_WHITELIST = (
       'localhost:8080',
